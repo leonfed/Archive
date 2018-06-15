b86f4f2db2ae666b2d0660518abf87cd898058cc
leonfed <31846280+leonfed@users.noreply.github.com>
2018-06-16 01:06:18 +0300

    Update archive.h

--- my_lib/archive.h
+++ my_lib/archive.h
@@ -13,21 +13,22 @@ struct archive {
 
     ~archive();
 
-    void count(unsigned char *, int);
+    void count(unsigned char* buf, int size_buf);
     void generate_code();
-    void  get_tree_code(std::vector<unsigned char>&);
-    void get_data_code(unsigned char*, int, std::vector<unsigned char>&);
+    void get_tree_code(std::vector<unsigned char>& code);
+    void get_data_code(unsigned char* buf, int size_buf, std::vector<unsigned char>& code);
 
 
 private:
     void dfs_delete(Node*);
-    void dfs_fill_byte_code(std::vector<bool>&, Node*);
-    void convert_to_char(std::vector<bool>& code_vector, std::vector<unsigned char>&);
+    void dfs_fill_byte_code(std::pair<unsigned long long, int>&, Node*);
+    void convert_bool_to_char(std::vector<bool>&, std::vector<unsigned char>&);
+    void convert_long_to_char(std::vector<std::pair<unsigned long long, int>>&, std::vector<unsigned char>&);
 
     std::vector<unsigned long long> cnt;
     Node* tree;
-    std::vector<std::vector<bool>> byte_code;
-    std::vector<bool> final_byte;
+    std::vector<std::pair<unsigned long long, int>> byte_code;
+    std::pair<unsigned long long, int> final_byte;
 };
 
 #endif
