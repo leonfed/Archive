123b803660608ff3ccdf0735f0cb58411a5c6337
leonfed <31846280+leonfed@users.noreply.github.com>
2018-06-16 01:06:36 +0300

    Update archive.cpp

--- my_lib/archive.cpp
+++ my_lib/archive.cpp
@@ -23,13 +23,13 @@ void archive::count(unsigned char *data, int length) {
     }
 }
 
-void archive::dfs_fill_byte_code(std::vector<bool>& cur_code, Node* v) {
+void archive::dfs_fill_byte_code(std::pair<unsigned long long, int>& cur_code, Node* v) {
     if (v->left) {
-        cur_code.push_back(0);
+        cur_code = {cur_code.first * 2, cur_code.second + 1};
         dfs_fill_byte_code(cur_code, v->left);
-        cur_code.back() = 1;
+        cur_code.first++;
         dfs_fill_byte_code(cur_code, v->right);
-        cur_code.pop_back();
+        cur_code = {cur_code.first / 2, cur_code.second - 1};
     } else {
         if (v->count != 0) {
             byte_code[v->byte] = cur_code;
@@ -56,7 +56,7 @@ void archive::generate_code() {
         nd.insert(new Node{ min1->count + min2->count, min1, min2, 0 });
     }
     tree = *nd.begin();
-    std::vector<bool> cur_code;
+    std::pair<unsigned long long, int> cur_code;
     dfs_fill_byte_code(cur_code, tree);
 }
 
@@ -71,20 +71,14 @@ void dfs_get_tree_code(std::vector<bool>& code, Node* v) {
         if (v->count > 0) {
             unsigned char byte = v->byte;
             for (int i = 0; i < 8; i++) {
-                code.push_back(byte & 1);
+                code.push_back(static_cast<bool>(byte & 1));
                 byte >>= 1;
             }
         }
     }
 }
 
-void archive::get_tree_code(std::vector<unsigned char>& code_char) {
-    std::vector<bool> code;
-    dfs_get_tree_code(code, tree);
-    convert_to_char(code, code_char);
-}
-
-void archive::convert_to_char(std::vector<bool>& code_vector, std::vector<unsigned char>& code_char) {
+void archive::convert_bool_to_char(std::vector<bool>& code_vector, std::vector<unsigned char>& code_char) {
     int sz = (int)code_vector.size() / 8;
     unsigned char cur;
     for (int i = 0; i < sz * 8; i += 8) {
@@ -104,15 +98,46 @@ void archive::convert_to_char(std::vector<bool>& code_vector, std::vector<unsign
     }
 }
 
+void archive::get_tree_code(std::vector<unsigned char>& code_char) {
+    std::vector<bool> code;
+    dfs_get_tree_code(code, tree);
+    convert_bool_to_char(code, code_char);
+}
+
 void archive::get_data_code(unsigned char* data, int length, std::vector<unsigned char>& code_char) {
-    std::vector<bool> code_vector;
+    std::vector<std::pair<unsigned long long, int>> code_vector;
     for (int i = 0; i < length; i++) {
-        for (bool b : byte_code[data[i]]) {
-            code_vector.push_back(b);
+        code_vector.push_back(byte_code[data[i]]);
+    }
+    code_vector.push_back(final_byte);
+    convert_long_to_char(code_vector, code_char);
+}
+
+
+
+void archive::convert_long_to_char(std::vector<std::pair<unsigned long long, int>>& code_vector, std::vector<unsigned char>& code_char) {
+    int rest = 8;
+    unsigned char cur = 0;
+    for (std::pair<unsigned long long, int> p : code_vector) {
+        unsigned long long num = p.first;
+        int cnt = p.second;
+        while (cnt >= rest) {
+            cur <<= rest;
+            cur += (num >> (cnt - rest));
+            code_char.push_back(cur);
+            cnt -= rest;
+            num &= (1 << (cnt + 1)) - 1;
+            rest = 8;
+            cur = 0;
+        }
+        if (cnt > 0) {
+            cur <<= cnt;
+            cur += num;
+            rest -= cnt;
         }
     }
-    for (bool b : final_byte) {
-        code_vector.push_back(b);
+    if (rest > 0 && rest != 8) {
+        cur <<= rest;
+        code_char.push_back(cur);
     }
-    convert_to_char(code_vector, code_char);
 }
